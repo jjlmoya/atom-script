e7412d5d146180a154a359f691bf722783777f3b
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-04-01 22:53:03 +0200

    Anchor Cloud

--- service/heading.js
+++ service/heading.js
@@ -1,12 +1,15 @@
 module.exports = {
-    slugify: function(str) {
+    locators: {
+        container: 'bs_anchor_container'
+    },
+    slugify: function (str) {
         str = str.replace(/^\s+|\s+$/g, ''); // trim
         str = str.toLowerCase();
 
         // remove accents, swap ñ for n, etc
         var from = "ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆÍÌÎÏŇÑÓÖÒÔÕØŘŔŠŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇíìîïňñóöòôõøðřŕšťúůüùûýÿžþÞĐđßÆa·/_,:;";
-        var to   = "AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAa------";
-        for (var i=0, l=from.length ; i<l ; i++) {
+        var to = "AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAa------";
+        for (var i = 0, l = from.length; i < l; i++) {
             str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
         }
 
@@ -17,11 +20,30 @@ module.exports = {
         return str;
     },
     renderAnchors: function (elements, prepend) {
-        console.log(elements);
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].id = this.slugify(elements[i].innerText);
-            elements[i].innerHTML = prepend + elements[i].innerHTML;
+        var containerHTML = '',
+            container = document.getElementsByClassName(this.locators.container),
+            settings = container.length > 0 ? container[0].dataset : {};
+        if (!elements.length && container.length > 0) {
+            var parent = container[0].closest('.' + settings.parent);
+            parent.style.display = 'none';
         }
+        for (var i = 0; i < elements.length; i++) {
+            var id = this.slugify(elements[i].innerText),
+                html = elements[i].innerHTML;
+            elements[i].id = id;
+            elements[i].innerHTML = prepend + html;
+            containerHTML += this.anchorLink(id, html, settings);
+        }
+        this.fillContainer(container, containerHTML);
+    },
+    fillContainer: function (container, html) {
+        if (container.length > 0) {
+            container[0].innerHTML = html;
+        }
+    },
+    anchorLink: function (id, html, settings) {
+        var linkClass = settings.linkClass;
+        return '<a class="' + linkClass + '" href="#' + id + '">' + html + '</a>';
     },
     getHeadingElements: function (selector) {
         return document.querySelectorAll(selector);
@@ -29,5 +51,6 @@ module.exports = {
     init: function (selector, prepend) {
         var elements = this.getHeadingElements(selector);
         this.renderAnchors(elements, prepend);
+
     }
 };
