7979b651d77400065f50a3f6f8111a946ac4a7c6
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-04-09 21:16:44 +0200

    Generic Slider

--- service/slider.js
+++ service/slider.js
@@ -1,14 +1,50 @@
 module.exports = {
     locators: {
-        trigger: '.bs_slider'
+        parent: '.bs_slider',
+        content: '.bs_slider_content',
+        slide: '.bs_slide',
+        button: '.bs_slider_button'
     },
-    bindScrollEvents: (trigger) => {
-        var sliders = document.querySelectorAll(trigger);
+    bindScrollEvents: function () {
+        var that = this,
+            sliders = document.querySelectorAll(this.locators.content),
+            slideClass = this.locators.slide;
         sliders.forEach(function (slider) {
             slider.addEventListener('scroll', function (e) {
-                console.log('scrolling element: %o', e);
+                let slides = e.target.querySelectorAll(slideClass),
+                    buttons = that.getSliderButtons(e.target) || [],
+                    activeIndex = that.getActiveSlider(slides, e.target);
+                that.removeActiveClass(that.locators.slide);
+                slides[activeIndex].classList.add('is-active');
+                if (buttons.length > 0) {
+                    that.removeActiveClass(that.locators.button);
+                    buttons[activeIndex].classList.add('is-active');
+                }
             });
         });
+    },
+
+    removeActiveClass: function (locator) {
+        const element = document.querySelector(locator + '.is-active');
+        if (!!element) {
+            element.classList.remove('is-active');
+        }
+
+    },
+
+    getSliderButtons: function (target) {
+        const parent = target.closest(this.locators.parent);
+        if (!!parent) {
+            return parent.querySelectorAll(this.locators.button);
+        }
+    },
+    getActiveSlider: function (slides, parent) {
+        const slidesLength = slides.length,
+            scrolled = parent.scrollLeft,
+            totalScroll = parent.scrollWidth,
+            slideWidth = totalScroll / slidesLength;
+        return Math.floor(scrolled / (slideWidth - (slideWidth / 10)));
+
     },
     init: function () {
         this.bindScrollEvents(this.locators.trigger);
