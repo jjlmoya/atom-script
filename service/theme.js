ac7b579415898734a7c6697a15d4dbc0453e8088
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-05-13 06:58:39 +0200

    Add Theme Change Support

---
+++ service/theme.js
@@ -0,0 +1,62 @@
+module.exports = {
+    locators: {
+        trigger: 'bs_theme'
+    },
+    model: [],
+    renderControl: function (themes) {
+        let renderElements = themes.map((theme) => {
+            return `<div class="${theme} a-bg bs_theme_button" 
+                        data-theme="${theme}"
+                        style="width:25px; height: 25px; border-radius: 50%"></div>`;
+        }).join('');
+        return `<div class="l-position--absolute l-flex">${renderElements}</div>`;
+    },
+    getThemes: function (next) {
+        next(
+            [
+                'aviator',
+                'diable',
+                'forest',
+                'graylord',
+                'kino',
+                'lime-sports',
+                'lollipop',
+                'mint',
+                'paradise-orange',
+                'purple-skies',
+                'reverse-sky',
+                'sky'
+            ]);
+    },
+
+    removeThemes: function (element) {
+        this.model.themes.forEach((theme) => {
+            element.classList.remove(theme);
+        });
+    },
+    actionTheme: function (e) {
+        var parent = e.target.closest('.' + this.locators.trigger);
+        this.removeThemes(parent);
+        parent.classList.add(e.target.dataset.theme);
+    },
+    addColorThemeAction: function () {
+        var that = this, elements = document.querySelectorAll('.bs_theme_button');
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].addEventListener('click', that.actionTheme);
+        }
+    },
+    init: function () {
+        let elements = document.getElementsByClassName(this.locators.trigger);
+        if (!elements || elements.length === 0) return;
+        this.getThemes((themes) => {
+            let html = this.renderControl(themes);
+            this.model.themes = themes;
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].classList.add('l-position');
+                var wrapper = document.createElement('div').innerHTML = elements[i].innerHTML + html;
+                elements[i].innerHTML = wrapper;
+            }
+            this.addColorThemeAction();
+        });
+    }
+};
