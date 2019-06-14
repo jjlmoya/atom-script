f0c775615917257dca1bac6816c81e141fba76f2
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-06-14 21:23:05 +0200

    Remove spaces

--- utils/misc.js
+++ utils/misc.js
@@ -26,10 +26,8 @@ module.exports = {
             obj = {},
             pair,
             i;
-
         for (i in pairs) {
             if (pairs[i] === "") continue;
-
             pair = pairs[i].split("=");
             obj[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
         }
