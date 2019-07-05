b98f87c5996490f75ac16caaee5d5f331f67e9a0
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-07-05 15:22:23 +0200

    Exit Intent to ES6

--- apps.js
+++ apps.js
@@ -2,17 +2,15 @@ import './state/index';
 import './slider/index';
 import './tracking/index';
 import './social/index';
-
 import './theme/index';
+import './social/index';
+import './utils/exitIntent';
 
 (() => {
     const initUtils = () => {
-        require('./utils/socialMedia').init();
-        require('./utils/exitIntent').init();
         require('./utils/replace').init();
     }, initServices = () => {
         require('./service/heading').init('.bs_anchor h2', '<span class="a-text--shadow a-text--uppercase a-pad--x-5">#</span>');
-        require('./service/theme').init();
     };
     initUtils();
     initServices();
--- utils/exitIntent.js
+++ utils/exitIntent.js
@@ -1,49 +1,46 @@
-module.exports = {
-    locators: {
-        trigger: 'bs_exit_intent'
-    },
-    model: {
-        localStorage: 'budget-exit-intent',
-        dayInMilisec: 24 * 60 * 60 * 1000
-    },
-
-    isMouseOutside: function (e) {
-        return e.clientY < 0;
-    },
-
-    getShowedDay: function () {
-        var registeredDate = localStorage.getItem(this.model.localStorage);
-        console.log(typeof(registeredDate));
-        registeredDate = registeredDate ?
-            new Date(parseInt(registeredDate)) : new Date(0);
-        return registeredDate;
-    },
-
-    isDateOldEnough: function (today, register, maxDays) {
-        return (today.getTime() - register.getTime()) / (this.model.dayInMilisec * maxDays) > 1;
-    },
-    modalRulesPass: function (settings) {
-        return this.isDateOldEnough(new Date(), this.getShowedDay(), settings.every);
-    },
-    triggerExitIntent: function (element) {
-        var settings = element.dataset;
-        var that = this;
-        document.addEventListener("mouseleave", function (event) {
-            if (that.isMouseOutside(event) &&
-                that.modalRulesPass(settings)) {
-                localStorage.setItem('budget-exit-intent', new Date().getTime());
-                setTimeout(function () {
-                    document.getElementsByClassName(settings.popup)[0].classList.add('is-active');
-                    document.body.classList.add('overflow-blocked');
-                }, settings.exitDelay);
-
-            }
-        }, false);
-    },
-    init: function () {
-        var elements = document.getElementsByClassName(this.locators.trigger);
-        if (elements.length > 0) {
-            this.triggerExitIntent(elements[0]);
-        }
-    },
+const locators = {
+    trigger: 'bs_exit_intent'
 };
+const model = {
+    localStorage: 'budget-exit-intent',
+    dayInMilisec: 24 * 60 * 60 * 1000
+};
+
+const isMouseOutside = (e) => {
+    return e.clientY < 0;
+};
+
+const getShowedDay = () => {
+    let registeredDate = localStorage.getItem(model.localStorage);
+    registeredDate = registeredDate ?
+        new Date(parseInt(registeredDate)) : new Date(0);
+    return registeredDate;
+};
+
+const isDateOldEnough = (today, register, maxDays) => (today.getTime() - register.getTime()) / (model.dayInMilisec * maxDays) > 1;
+
+const modalRulesPass = settings => isDateOldEnough(new Date(), getShowedDay(), settings.every);
+
+const onMouseLeave = (event, settings) => {
+    if (isMouseOutside(event) &&
+        modalRulesPass(settings)) {
+        localStorage.setItem(model.localStorage, new Date().getTime());
+        setTimeout(() => {
+            document.getElementsByClassName(settings.popup)[0].classList.add('is-active');
+            document.body.classList.add('overflow-blocked');
+        }, settings.exitDelay);
+
+    }
+};
+const triggerExitIntent = element => {
+    document.addEventListener("mouseleave", (event) => {
+        onMouseLeave(event, element.dataset)
+    }, false);
+};
+
+(() => {
+    triggerExitIntent([...document.getElementsByClassName(locators.trigger)].slice(0, 1));
+})();
+
+
+module.exports = {};
