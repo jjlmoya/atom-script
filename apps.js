373cac25df085799ff83fe412c8d7cf09190ad2a
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-07-06 08:52:03 +0200

    Replace Image to ES6

--- apps.js
+++ apps.js
@@ -5,14 +5,12 @@ import './social/index';
 import './theme/index';
 import './social/index';
 import './utils/exitIntent';
+import './image/replace';
 
 (() => {
-    const initUtils = () => {
-        require('./utils/replace').init();
-    }, initServices = () => {
+    const initServices = () => {
         require('./service/heading').init('.bs_anchor h2', '<span class="a-text--shadow a-text--uppercase a-pad--x-5">#</span>');
     };
-    initUtils();
     initServices();
 })();
 
---
+++ image/replace.js
@@ -0,0 +1,25 @@
+const locators = {
+    image: '.bs_image_replace'
+};
+
+const replaceImage = (target, element) => {
+    document.querySelector(target).src = element.src
+};
+
+const onImageClick = e => {
+    const element = e.target,
+        targetClass = element.dataset.target;
+    if (targetClass) {
+        replaceImage(targetClass, element);
+    }
+};
+const bindReplace = (elements) => {
+    elements.forEach((element) => {
+        element.addEventListener('click', onImageClick);
+    });
+};
+
+(() => {
+    bindReplace(document.querySelectorAll(locators.image));
+})();
+
