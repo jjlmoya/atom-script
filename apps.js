d9498c6a07b585e0c9bf99af3f7085f4b5397061
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-07-07 20:15:46 +0200

    Replace Image to ES6

--- apps.js
+++ apps.js
@@ -6,10 +6,10 @@ import './theme/index';
 import './social/index';
 import './utils/exitIntent';
 import './image/replace';
-
 (() => {
     const initServices = () => {
-        require('./service/heading').init('.bs_anchor h2', '<span class="a-text--shadow a-text--uppercase a-pad--x-5">#</span>');
+        require('./service/heading')
+            .init('.bs_anchor h2', '<span class="a-text--shadow a-text--uppercase a-pad--x-5">#</span>');
     };
     initServices();
 })();
