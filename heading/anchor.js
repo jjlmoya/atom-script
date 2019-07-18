95bc213fbdf725b0d69f7384f2745f67e32dfafd
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-07-18 21:11:01 +0200

    Fix Anchor Position

--- heading/anchor.js
+++ heading/anchor.js
@@ -8,29 +8,30 @@ const renderAnchors = (elements, prepend) => {
     fillContainer(document.getElementsByClassName(locators.container), elements, prepend);
 };
 
-const fillContainer = (containers, elements, prepend) => {
+const fillContainer = (containers, elements) => {
     [...containers].forEach((container) => {
         let settings = container.dataset;
+
         container.innerHTML = [...elements].map((element) => {
-            let slug = stringToSlug(element.innerText),
-                html = element.innerHTML;
-            element.id = slug;
-            element.innerHTML = prepend + html;
+            let prepend = document.createElement('SPAN'),
+                slug = stringToSlug(element.innerText),
+                html = element.innerText;
+            prepend.classList.add('a-text--shadow','a-text--uppercase', 'a-pad--x-5', 'l-position--absolute', 'l-position--absolute--anchor');
+            prepend.id = slug;
+            element.innerHTML = html;
+            element.append(prepend);
+            element.classList.add('l-position');
             return anchorLink(slug, html, settings.linkClass);
         }).join(' ');
 
     });
 };
 
-const anchorLink = (id, html, linkClass) => {
-    return `<a class="${linkClass}" href="#${id}">${html}</a>`;
-};
+const anchorLink = (id, html, linkClass) => `<a class="${linkClass}" href="#${id}">${html}</a>`;
 
 const getHeadingElements = selectors => document.querySelectorAll(selectors);
 
 (() => {
-    let selector = '.bs_anchor h2',
-        prepend = '<span class="a-text--shadow a-text--uppercase a-pad--x-5"></span>';
-    var elements = getHeadingElements(selector);
-    renderAnchors(elements, prepend);
+    let selector = '.bs_anchor h2, .bs_anchor h3';
+    renderAnchors(getHeadingElements(selector));
 })();
