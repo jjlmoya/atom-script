dde04b96b454e50273a5903b7ab70dd95e6acf83
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-07-22 15:29:57 +0200

    Add services mock data

--- project/mv/services.js
+++ project/mv/services.js
@@ -41,6 +41,56 @@ const subcategories = [
     }
 ];
 
+const offers = [
+    {
+        "bhc": 23926,
+        "name": "Comfort Inn",
+        "gym": 58,
+        "climatizedpool": 101
+    },
+    {
+        "bhc": 23927,
+        "name": "Rodeway Inn"
+    },
+    {
+        "bhc": 23928,
+        "name": "Magic Pas",
+        "gym": 58
+    },
+    {
+        "bhc": 23930,
+        "name": "Econo Lodge, Main Street, Marion"
+    },
+    {
+        "bhc": 23931,
+        "name": "Comfort Inn & Suites",
+        "gym": 58,
+        "climatizedpool": 101
+    },
+    {
+        "bhc": 23368,
+        "name": "Jutlandia"
+    },
+    {
+        "bhc": 23369,
+        "name": "Crowne Plaza Fredericton Lord Beaverbrook",
+        "gym": 58,
+        "climatizedpool": 101,
+        "seapool": 184
+    },
+    {
+        "bhc": 23371,
+        "name": "Holiday Inn Express Whitby Oshawa",
+        "gym": 58
+    },
+    {
+        "bhc": 23372,
+        "name": "Kaikoura Gateway Motor Lodge"
+    }
+];
+
 export const GetCategories = () => categories;
 
 export const GetSubcategories = () => subcategories;
+
+export const GetOffers = () => offers;
